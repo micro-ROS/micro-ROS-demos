3c26d580f9be06ca0962c7f000f9a5bd6bf83a58
Javier Moreno <javiermoreno@eprosima.com>
2018-09-26 08:46:58 +0200

    added tests: mesgs, sub and pub

--- example_publisher/main.c
+++ example_publisher/main.c
@@ -1,5 +1,5 @@
 #include <rclc/rclc.h>
-#include <std_msgs/msg/string.h>
+#include <example_custom_msgs/msg/nested_msg_test.h>
 
 #include <stdio.h>
 
@@ -10,22 +10,41 @@ int main(int argc, char* argv[])
     rclc_init(0, NULL);
     rclc_node_t* node = rclc_create_node("publisher_node", "");
     rclc_publisher_t* publisher =
-        rclc_create_publisher(node, RCLC_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "publisher_example", 1);
+        rclc_create_publisher(node, RCLC_GET_MSG_TYPE_SUPPORT(example_custom_msgs, msg, NestedMsgTest), "example_custom_msgs_msg_NestedMsgTest", 1);
 
-    std_msgs__msg__String msg;
-    char buff[128]    = {0};
-    msg.data.data     = buff;
-    msg.data.capacity = sizeof(buff);
-    msg.data.size     = 0;
+    example_custom_msgs__msg__NestedMsgTest msg;
+    char Buff1[30];
+    msg.data14.data1.data = Buff1;
+    msg.data14.data1.size = 0;
+    msg.data14.data1.capacity = sizeof(Buff1);
+
+    char Buff2[30];
+    msg.data14.data2.data = Buff2;
+    msg.data14.data2.size = 0;
+    msg.data14.data2.capacity = sizeof(Buff1);
+    
+    char Buff3[30];
+    msg.data14.data3.data = Buff3;
+    msg.data14.data3.size = 0;
+    msg.data14.data3.capacity = sizeof(Buff1);
+    
+    char Buff4[30];
+    msg.data14.data4.data = Buff4;
+    msg.data14.data4.size = 0;
+    msg.data14.data4.capacity = sizeof(Buff1);
+    
+    
 
     int num = 0;
     while (rclc_ok())
     {
-        msg.data.size = snprintf(msg.data.data, msg.data.capacity, "Hello World %i", num++);
-        if (msg.data.size > msg.data.capacity)
-            msg.data.size = 0;
+        msg.data14.data1.size = snprintf(msg.data14.data1.data, msg.data14.data1.capacity, "1 - %i", num++);
+        msg.data14.data2.size = snprintf(msg.data14.data2.data, msg.data14.data2.capacity, "2 - %i", num++);
+        msg.data14.data3.size = snprintf(msg.data14.data3.data, msg.data14.data3.capacity, "3 - %i", num++);
+        msg.data14.data4.size = snprintf(msg.data14.data4.data, msg.data14.data4.capacity, "4 - %i", num++);
 
-        printf("Sending: '%s'\n", msg.data.data);
+
+        printf("Sending (%i): '%s\t%s\t%s\t%s'\n", sizeof(msg), msg.data14.data1.data, msg.data14.data2.data, msg.data14.data3.data, msg.data14.data4.data);
         rclc_publish(publisher, (const void*)&msg);
         rclc_spin_node_once(node, 500);
     }
--- example_subscriber/main.c
+++ example_subscriber/main.c
@@ -1,12 +1,12 @@
 #include <rclc/rclc.h>
-#include <std_msgs/msg/string.h>
+#include <example_custom_msgs/msg/nested_msg_test.h>
 
 #include <stdio.h>
 
 void on_message(const void* msgin)
 {
-    const std_msgs__msg__String* msg = (const std_msgs__msg__String*)msgin;
-    printf("I heard: [%s]\n", msg->data.data);
+    const example_custom_msgs__msg__NestedMsgTest* msg = (const example_custom_msgs__msg__NestedMsgTest*)msgin;
+    printf("I heard: [%s\t%s\t%s\t%s]\n", msg->data14.data1.data, msg->data14.data2.data, msg->data14.data3.data, msg->data14.data4.data);
 }
 
 int main(int argc, char* argv[])
@@ -15,8 +15,7 @@ int main(int argc, char* argv[])
     (void)argv;
     rclc_init(0, NULL);
     rclc_node_t* node        = rclc_create_node("subscription_node", "");
-    rclc_subscription_t* sub = rclc_create_subscription(node, RCLC_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-                                                        "subscription_example", on_message, 1, false);
+    rclc_subscription_t* sub = rclc_create_subscription(node, RCLC_GET_MSG_TYPE_SUPPORT(example_custom_msgs, msg, NestedMsgTest), "example_custom_msgs_msg_NestedMsgTest", on_message, 1, false);
 
     rclc_spin_node(node);
 
