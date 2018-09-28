462c219d2bd4310c1d93a0fa07a1eca6bb5fe3fe
Javier Moreno <javiermoreno@eprosima.com>
2018-09-28 09:57:29 +0200

    Change examples in order to be more explanatory

--- example_publisher/main.c
+++ example_publisher/main.c
@@ -38,13 +38,47 @@ int main(int argc, char* argv[])
     int num = 0;
     while (rclc_ok())
     {
-        msg.data14.data1.size = snprintf(msg.data14.data1.data, msg.data14.data1.capacity, "1 - %i", num++);
-        msg.data14.data2.size = snprintf(msg.data14.data2.data, msg.data14.data2.capacity, "2 - %i", num++);
-        msg.data14.data3.size = snprintf(msg.data14.data3.data, msg.data14.data3.capacity, "3 - %i", num++);
-        msg.data14.data4.size = snprintf(msg.data14.data4.data, msg.data14.data4.capacity, "4 - %i", num++);
+        msg.data1 = (bool)(num & 0x01);
+        msg.data2 = (uint8_t)num;
+        msg.data3 = (signed char)num;
+        msg.data4 = (float)num;
+        msg.data5 = (double)num;
+        msg.data6 = (int8_t)num;
+        msg.data7 = (uint8_t)num;
+        msg.data8 = (int16_t)num;
+        msg.data9 = (uint16_t)num;
+        msg.data10 = (int32_t)num;
+        msg.data11 = (uint32_t)num;
+        msg.data12 = (int64_t)num;
+        msg.data13 = (uint64_t)num;
+        msg.data14.data1.size = snprintf(msg.data14.data1.data, msg.data14.data1.capacity, "Msg A - %i", num);
+        msg.data14.data2.size = snprintf(msg.data14.data2.data, msg.data14.data2.capacity, "Msg B - %i", num);
+        msg.data14.data3.size = snprintf(msg.data14.data3.data, msg.data14.data3.capacity, "Msg C - %i", num);
+        msg.data14.data4.size = snprintf(msg.data14.data4.data, msg.data14.data4.capacity, "Msg D - %i", num);
+        num++;
 
 
-        printf("Sending (%i): '%s\t%s\t%s\t%s'\n", sizeof(msg), msg.data14.data1.data, msg.data14.data2.data, msg.data14.data3.data, msg.data14.data4.data);
+        printf("I send:\n");
+        printf("\tBool: %u\n", msg.data1);
+        printf("\tuint8_t: %u\n", msg.data2);
+        printf("\tsigned char: %u\n", msg.data3);
+        printf("\tfloat: %f\n", msg.data4);
+        printf("\tdouble: %lf\n", msg.data5);
+        printf("\tint8_t: %i\n", msg.data6);
+        printf("\tuint8_t: %u\n", msg.data7);
+        printf("\tint16_t: %i\n", msg.data8);
+        printf("\tuint16_t: %u\n", msg.data9);
+        printf("\tint32_t: %i\n", msg.data10);
+        printf("\tuint32_t: %u\n", msg.data11);
+        printf("\tint64_t: %li\n", msg.data12);
+        printf("\tuint64_t: %lu\n", msg.data13);
+
+        printf("\tstring 1: %s\n", msg.data14.data1.data);
+        printf("\tstring 2: %s\n", msg.data14.data2.data);
+        printf("\tstring 3: %s\n", msg.data14.data3.data);
+        printf("\tstring 4: %s\n", msg.data14.data4.data);
+        printf("\n\n");
+
         rclc_publish(publisher, (const void*)&msg);
         rclc_spin_node_once(node, 500);
     }
--- example_subscriber/main.c
+++ example_subscriber/main.c
@@ -6,7 +6,26 @@
 void on_message(const void* msgin)
 {
     const example_custom_msgs__msg__NestedMsgTest* msg = (const example_custom_msgs__msg__NestedMsgTest*)msgin;
-    printf("I heard: [%s\t%s\t%s\t%s]\n", msg->data14.data1.data, msg->data14.data2.data, msg->data14.data3.data, msg->data14.data4.data);
+    printf("I heard:\n");
+    printf("\tBool: %u\n", msg->data1);
+    printf("\tuint8_t: %u\n", msg->data2);
+    printf("\tsigned char: %u\n", msg->data3);
+    printf("\tfloat: %f\n", msg->data4);
+    printf("\tdouble: %lf\n", msg->data5);
+    printf("\tint8_t: %i\n", msg->data6);
+    printf("\tuint8_t: %u\n", msg->data7);
+    printf("\tint16_t: %i\n", msg->data8);
+    printf("\tuint16_t: %u\n", msg->data9);
+    printf("\tint32_t: %i\n", msg->data10);
+    printf("\tuint32_t: %u\n", msg->data11);
+    printf("\tint64_t: %li\n", msg->data12);
+    printf("\tuint64_t: %lu\n", msg->data13);
+
+    printf("\tstring 1: %s\n", msg->data14.data1.data);
+    printf("\tstring 2: %s\n", msg->data14.data2.data);
+    printf("\tstring 3: %s\n", msg->data14.data3.data);
+    printf("\tstring 4: %s\n", msg->data14.data4.data);
+    printf("\n\n");
 }
 
 int main(int argc, char* argv[])
