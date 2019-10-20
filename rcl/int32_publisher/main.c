7d0507591e52c8650fbfb32551794279a6c7c14d
Borja Outerelo <borjaouterelo@gmail.com>
2019-10-20 19:44:28 +0200

    Add rcl error print and fix subscription.

--- rcl/int32_publisher/main.c
+++ rcl/int32_publisher/main.c
@@ -1,4 +1,5 @@
 #include <rcl/rcl.h>
+#include <rcl/error_handling.h>
 #include <std_msgs/msg/int32.h>
 
 #include <stdio.h>
@@ -11,22 +12,22 @@ int main(int argc, const char * const * argv)
   rcl_init_options_t options = rcl_get_zero_initialized_init_options();
   rv = rcl_init_options_init(&options, rcl_get_default_allocator());
   if (RCL_RET_OK != rv) {
-    printf("rcl init options error\n");
+    printf("rcl init options error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rcl_context_t context = rcl_get_zero_initialized_context();
   rv = rcl_init(argc, argv, &options, &context);
   if (RCL_RET_OK != rv) {
-    printf("rcl initialization error\n");
+    printf("rcl initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rcl_node_options_t node_ops = rcl_node_get_default_options();
-  rcl_node_t node;
+  rcl_node_t node = rcl_get_zero_initialized_node();
   rv = rcl_node_init(&node, "int32_publisher_rcl", "", &context, &node_ops);
   if (RCL_RET_OK != rv) {
-    printf("Node initialization error\n");
+    printf("Node initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
@@ -34,16 +35,16 @@ int main(int argc, const char * const * argv)
   rcl_publisher_t publisher = rcl_get_zero_initialized_publisher();
   rv = rcl_publisher_init(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &publisher_ops);
   if (RCL_RET_OK != rv) {
-    printf("Publisher initialization error\n");
+    printf("Publisher initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
-  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
+ /* rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
   rv = rcl_wait_set_init(&wait_set, 0, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
   if (RCL_RET_OK != rv) {
-    printf("Wait set initialization error\n");
+    printf("Wait set initialization error: %s\n", rcl_get_error_string().str);
     return 1;
-  }
+  }*/
 
   std_msgs__msg__Int32 msg;
   const int num_msg = 1000;
--- rcl/int32_subscriber/main.c
+++ rcl/int32_subscriber/main.c
@@ -6,24 +6,24 @@
 
 int main(int argc, const char * const * argv)
 {
-  rcl_ret_t rv;
+  rcl_ret_t rv = RCL_RET_ERROR;
 
   rcl_init_options_t options = rcl_get_zero_initialized_init_options();
   rv = rcl_init_options_init(&options, rcl_get_default_allocator());
   if (RCL_RET_OK != rv) {
-    printf("rcl init options error\n");
+    printf("rcl init options error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rcl_context_t context = rcl_get_zero_initialized_context();
   rv = rcl_init(argc, argv, &options, &context);
   if (RCL_RET_OK != rv) {
-    printf("rcl initialization error\n");
+    printf("rcl initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rcl_node_options_t node_ops = rcl_node_get_default_options();
-  rcl_node_t node;
+  rcl_node_t node = rcl_get_zero_initialized_node();
   rv = rcl_node_init(&node, "int32_subscriber_rcl", "", &context, &node_ops);
   if (RCL_RET_OK != rv)
   {
@@ -37,33 +37,33 @@ int main(int argc, const char * const * argv)
   rv = rcl_subscription_init(
     &subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "std_msgs_msg_Int32", &subscription_ops);
   if (RCL_RET_OK != rv) {
-    printf("Subscription initialization error\n");
+    printf("Subscription initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
   rv = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
   if (RCL_RET_OK != rv) {
-    printf("Wait set initialization error\n");
+    printf("Wait set initialization error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   rv = rcl_wait_set_clear(&wait_set);
   if (RCL_RET_OK != rv) {
-    printf("Wait set clear error\n");
+    printf("Wait set clear error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   size_t index;
   rv = rcl_wait_set_add_subscription(&wait_set, &subscription, &index);
   if (RCL_RET_OK != rv) {
-    printf("Wait set add subscription error\n");
+    printf("Wait set add subscription error: %s\n", rcl_get_error_string().str);
     return 1;
   }
 
   void* msg = rcl_get_default_allocator().zero_allocate(sizeof(std_msgs__msg__Int32), 1, rcl_get_default_allocator().state);
   do {
-    rv &= rcl_wait(&wait_set, -1);
+    rv = rcl_wait(&wait_set, 1000000);
     for (size_t i = 0; i < wait_set.size_of_subscriptions; ++i) {
       rv = rcl_take(wait_set.subscriptions[i], msg, NULL, NULL);
       if (RCL_RET_OK == rv)
@@ -71,7 +71,7 @@ int main(int argc, const char * const * argv)
         printf("I received: [%i]\n", ((const std_msgs__msg__Int32*)msg)->data);
       }
     }
-  } while ( RCL_RET_OK == rv );
+  } while ( RCL_RET_OK == rv || RCL_RET_SUBSCRIPTION_TAKE_FAILED == rv);
 
   rv = rcl_subscription_fini(&subscription, &node);
   rv = rcl_node_fini(&node);
