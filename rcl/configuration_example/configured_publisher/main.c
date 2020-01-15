086be097e4a09a0953c00689de7b458eb3dc073d
Pablo Garrido <pablogs9@gmail.com>
2020-01-15 16:28:28 +0100

    Examples cleanup ad error code check

--- rcl/configuration_example/configured_publisher/main.c
+++ rcl/configuration_example/configured_publisher/main.c
@@ -22,29 +22,27 @@
 
 int main(int argc, char * const argv[])
 {
-  rcl_ret_t ret;
-
   //Init RCL context
   rcl_context_t context;
   rcl_init_options_t init_options;
 
   init_options = rcl_get_zero_initialized_init_options();
-  ret = rcl_init_options_init(&init_options, rcl_get_default_allocator());
+  RCCHECK(rcl_init_options_init(&init_options, rcl_get_default_allocator()))
   
   printf("Connecting to agent %s:%d\n",argv[1],atoi(argv[2]));
   rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
-  rmw_uros_options_set_udp_address(argv[1], argv[2], rmw_options);
-  rmw_uros_options_set_client_key(0xBA5EBA11, rmw_options);
+  RCCHECK(rmw_uros_options_set_udp_address(argv[1], argv[2], rmw_options))
+  RCCHECK(rmw_uros_options_set_client_key(0xBA5EBA11, rmw_options))
 
   context = rcl_get_zero_initialized_context();
 
-  ret = rcl_init(0, NULL, &init_options, &context);
+  RCCHECK(rcl_init(0, NULL, &init_options, &context))
 
   //Init Node
   rcl_node_t node = rcl_get_zero_initialized_node();
   rcl_node_options_t node_ops = rcl_node_get_default_options();
 
-  ret = rcl_node_init(&node, "vector3_publisher", "", &context, &node_ops);
+  RCCHECK(rcl_node_init(&node, "vector3_publisher", "", &context, &node_ops))
 
   //Init Publisher
   const char* topic_name = "sample_vector3";
@@ -53,29 +51,28 @@ int main(int argc, char * const argv[])
   const rosidl_message_type_support_t * pub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3);
   rcl_publisher_options_t pub_opt = rcl_publisher_get_default_options();
 
-  ret = rcl_publisher_init(&publisher_vector3, &node, pub_type_support, topic_name, &pub_opt);
+  RCCHECK(rcl_publisher_init(&publisher_vector3, &node, pub_type_support, topic_name, &pub_opt))
 
   geometry_msgs__msg__Vector3 topic_data;
   geometry_msgs__msg__Vector3__init(&topic_data);
 
   // Spin
   rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  ret = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
+  RCCHECK(rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator()))
 
   float values[3];
-  for(uint8_t i = 0; i < 3; i++){
-    values[i] = i;
-  }
+  values[0] = 0.0;
+  values[1] = 1.0;
+  values[2] = 2.0;
 
   topic_data.x = values[0];
   topic_data.y = values[1];
   topic_data.z = values[2];
 
-  while (rcl_context_is_valid(&context))
-  {     
-    ret = rcl_publish( &publisher_vector3, (const void *) &topic_data, NULL);
+  while (rcl_context_is_valid(&context)) {     
+    RCSOFTCHECK(rcl_publish( &publisher_vector3, (const void *) &topic_data, NULL))
     
-    for(uint8_t i = 0; i < 3; i++){
+    for(uint8_t i = 0; i < 3; i++) {
       values[i]++;
     }
 
@@ -84,10 +81,8 @@ int main(int argc, char * const argv[])
     topic_data.z = values[2];
 
     printf("Publish: %f, %f, %f\n",topic_data.x,topic_data.y,topic_data.z);
-
     sleep(1);
   }
   
-  ret = rcl_node_fini(&node);
-
+  RCCHECK(rcl_node_fini(&node))
 }
--- rcl/configuration_example/configured_subscriber/main.c
+++ rcl/configuration_example/configured_subscriber/main.c
@@ -22,29 +22,27 @@
 
 int main(int argc, char * argv[])
 {
-  rcl_ret_t ret;
-
   //Init RCL context
   rcl_context_t context;
   rcl_init_options_t init_options;
 
   init_options = rcl_get_zero_initialized_init_options();
-  ret = rcl_init_options_init(&init_options, rcl_get_default_allocator());
+  RCCHECK(rcl_init_options_init(&init_options, rcl_get_default_allocator()))
 
   printf("Connecting to agent %s:%d\n",argv[1],atoi(argv[2]));
   rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
-  rmw_uros_options_set_udp_address(argv[1], argv[2], rmw_options);
-  rmw_uros_options_set_client_key(0xCAFEBABE, rmw_options);
+  RCCHECK(rmw_uros_options_set_udp_address(argv[1], argv[2], rmw_options))
+  RCCHECK(rmw_uros_options_set_client_key(0xCAFEBABE, rmw_options))
 
   context = rcl_get_zero_initialized_context();
 
-  ret = rcl_init(0, NULL, &init_options, &context);
+  RCCHECK(rcl_init(0, NULL, &init_options, &context))
 
   //Init Node
   rcl_node_t node = rcl_get_zero_initialized_node();
   rcl_node_options_t node_ops = rcl_node_get_default_options();
 
-  ret = rcl_node_init(&node, "vector3_subscriber", "", &context, &node_ops);
+  RCCHECK(rcl_node_init(&node, "vector3_subscriber", "", &context, &node_ops))
 
   //Init Subscriber
   const char* topic_name = "sample_vector3";
@@ -53,14 +51,14 @@ int main(int argc, char * argv[])
   
   const rosidl_message_type_support_t * sub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3);
   
-  ret = rcl_subscription_init(&subscriber_vector3, &node, sub_type_support, topic_name, &subs_ops);
+  RCCHECK(rcl_subscription_init(&subscriber_vector3, &node, sub_type_support, topic_name, &subs_ops))
 
   geometry_msgs__msg__Vector3 topic_data;
   geometry_msgs__msg__Vector3__init(&topic_data);
 
   // Spin
   rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  ret = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
+  RCCHECK(rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator()))
 
   uint32_t timeout_ms = 500;
 
@@ -68,25 +66,25 @@ int main(int argc, char * argv[])
   {     
     // get empty wait set
     rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-    ret = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator());
+    RCSOFTCHECK(rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, 0, &context, rcl_get_default_allocator()))
 
     // set rmw fields to NULL
-    ret = rcl_wait_set_clear(&wait_set);
+    RCSOFTCHECK(rcl_wait_set_clear(&wait_set))
 
     size_t index = 0;
-    ret = rcl_wait_set_add_subscription(&wait_set, &subscriber_vector3, &index);
+    RCSOFTCHECK(rcl_wait_set_add_subscription(&wait_set, &subscriber_vector3, &index))
 
-    ret = rcl_wait(&wait_set, RCL_MS_TO_NS(timeout_ms));				
+    RCSOFTCHECK(rcl_wait(&wait_set, RCL_MS_TO_NS(timeout_ms)))				
 
     if (ret == RCL_RET_OK && wait_set.subscriptions[0]) {
         rmw_message_info_t messageInfo;
-        ret = rcl_take(wait_set.subscriptions[0], &topic_data, &messageInfo, NULL);
+        RCSOFTCHECK(rcl_take(wait_set.subscriptions[0], &topic_data, &messageInfo, NULL))
         printf("Received: %f, %f, %f\n",topic_data.x,topic_data.y,topic_data.z);
     }
 
     rcl_wait_set_fini(&wait_set);
   }
   
-  ret = rcl_node_fini(&node);
+  RCCHECK(rcl_node_fini(&node))
 
 }
