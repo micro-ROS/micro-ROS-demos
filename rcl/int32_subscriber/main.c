b98c16c6013218aa61e9e92571011df83a690ac4
Pablo Garrido <pablogs9@gmail.com>
2020-01-16 07:32:53 +0100

    Typo

--- rcl/int32_subscriber/main.c
+++ rcl/int32_subscriber/main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 int main(int argc, const char * const * argv)
 {
