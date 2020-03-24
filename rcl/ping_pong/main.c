2f766f6a08dd6edb8b623e1e7860442b3ab7fab7
Pablo Garrido <pablogs9@gmail.com>
2020-03-24 11:55:00 +0100

    Creating ping pong app

---
+++ rcl/ping_pong/main.c
@@ -0,0 +1,149 @@
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/header.h>
+
+#include <rmw_uros/options.h>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define STRING_BUFFER_LEN 100
+
+// Publishing ping guard condition thread
+void * trigger_guard_condition(void *args){
+  rcl_guard_condition_t * guard_condition = (rcl_guard_condition_t *)args;
+
+  while(true){
+    rcl_trigger_guard_condition(guard_condition);
+    sleep(5);
+  }
+}
+
+int main(int argc, const char * const * argv)
+{
+  //Init RCL options
+  rcl_init_options_t options = rcl_get_zero_initialized_init_options();
+  rcl_init_options_init(&options, rcl_get_default_allocator());
+  
+  // Init RCL context
+  rcl_context_t context = rcl_get_zero_initialized_context();
+  rcl_init(0, NULL, &options, &context);
+
+  // Create a node
+  rcl_node_options_t node_ops = rcl_node_get_default_options();
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  rcl_node_init(&node, "pingpong_node", "", &context, &node_ops);
+
+  // Create a reliable ping publisher
+  rcl_publisher_options_t ping_publisher_ops = rcl_publisher_get_default_options();
+  rcl_publisher_t ping_publisher = rcl_get_zero_initialized_publisher();
+  rcl_publisher_init(&ping_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping", &ping_publisher_ops);
+
+  // Create a best effort pong publisher
+  rcl_publisher_options_t pong_publisher_ops = rcl_publisher_get_default_options();
+  pong_publisher_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+  rcl_publisher_t pong_publisher = rcl_get_zero_initialized_publisher();
+  rcl_publisher_init(&pong_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong", &pong_publisher_ops);
+
+  // Create a best effort  pong subscriber
+  rcl_subscription_options_t pong_subscription_ops = rcl_subscription_get_default_options();
+  pong_subscription_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+  rcl_subscription_t pong_subscription = rcl_get_zero_initialized_subscription();
+  rcl_subscription_init(&pong_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong", &pong_subscription_ops);
+
+  // Create a best effort ping subscriber
+  rcl_subscription_options_t ping_subscription_ops = rcl_subscription_get_default_options();
+  ping_subscription_ops.qos.reliability = RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT;
+  rcl_subscription_t ping_subscription = rcl_get_zero_initialized_subscription();
+  rcl_subscription_init(&ping_subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping", &ping_subscription_ops);
+
+  // Create guard condition
+  rcl_guard_condition_t guard_condition = rcl_get_zero_initialized_guard_condition();
+  rcl_guard_condition_options_t guard_condition_options = rcl_guard_condition_get_default_options();
+  rcl_guard_condition_init(&guard_condition, &context, guard_condition_options);
+
+  pthread_t guard_condition_thread;
+  pthread_create(&guard_condition_thread, NULL, trigger_guard_condition, &guard_condition);
+
+  // Create a wait set
+  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
+  rcl_wait_set_init(&wait_set, 2, 1, 0, 0, 0, 0, &context, rcl_get_default_allocator());
+
+  // Create and allocate the pingpong publication message
+  std_msgs__msg__Header msg;
+  char msg_buffer[STRING_BUFFER_LEN];
+  msg.frame_id.data = msg_buffer;
+  msg.frame_id.capacity = STRING_BUFFER_LEN;
+
+  // Create and allocate the pingpong subscription message
+  std_msgs__msg__Header rcv_msg;
+  char rcv_buffer[STRING_BUFFER_LEN];
+  rcv_msg.frame_id.data = rcv_buffer;
+  rcv_msg.frame_id.capacity = STRING_BUFFER_LEN;
+
+  // Set device id and sequence number;
+  int device_id = rand();
+  int seq_no;
+  
+  int pong_count = 0;
+  struct timespec ts;
+  rcl_ret_t rc;
+
+    do {
+    // Clear and set the waitset
+    rcl_wait_set_clear(&wait_set);
+    
+    size_t index_pong_subscription;
+    rcl_wait_set_add_subscription(&wait_set, &pong_subscription, &index_pong_subscription);
+
+    size_t index_ping_subscription;
+    rcl_wait_set_add_subscription(&wait_set, &ping_subscription, &index_ping_subscription);
+
+    size_t index_guardcondition;
+    rcl_wait_set_add_guard_condition(&wait_set, &guard_condition, &index_guardcondition);
+    
+    // Run session for 100 ms
+    rcl_wait(&wait_set, RCL_MS_TO_NS(100));
+    
+    // Check if some pong message is received
+    if (wait_set.subscriptions[index_pong_subscription]) {
+      rc = rcl_take(wait_set.subscriptions[index_pong_subscription], &rcv_msg, NULL, NULL);
+      if(rc == RCL_RET_OK && strcmp(msg.frame_id.data,rcv_msg.frame_id.data) == 0) {
+          pong_count++;
+          printf("Pong for seq %s (%d)\n", rcv_msg.frame_id.data, pong_count);
+      }
+    }
+
+    // Check if some ping message is received and pong it
+    if (wait_set.subscriptions[index_ping_subscription]) {
+      rc = rcl_take(wait_set.subscriptions[index_ping_subscription], &rcv_msg, NULL, NULL);
+
+      // Dont pong my own pings
+      if(rc == RCL_RET_OK && strcmp(msg.frame_id.data,rcv_msg.frame_id.data) != 0){
+        printf("Ping received with seq %s. Answering.\n", rcv_msg.frame_id.data);
+        rcl_publish(&pong_publisher, (const void*)&rcv_msg, NULL);
+      }
+    }
+
+    // Check if it is time to send a ping
+    if (wait_set.guard_conditions[index_guardcondition]) {
+      // Generate a new random sequence number
+      seq_no = rand();
+      sprintf(msg.frame_id.data, "%d_%d", seq_no, device_id);
+      msg.frame_id.size = strlen(msg.frame_id.data);
+      
+      // Fill the message timestamp
+      clock_gettime(CLOCK_REALTIME, &ts);
+      msg.stamp.sec = ts.tv_sec;
+      msg.stamp.nanosec = ts.tv_nsec;
+
+      // Reset the pong count and publish the ping message
+      pong_count = 0;
+      rcl_publish(&ping_publisher, (const void*)&msg, NULL);
+      printf("Ping send seq %s\n", msg.frame_id.data);  
+    }
+    
+    usleep(10000);
+  } while (true);
+}
