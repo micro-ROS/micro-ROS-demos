fc0fe770f11c4666718edc72407773ff5c82a9bf
Pablo Garrido <pablogs9@gmail.com>
2020-05-29 09:44:09 +0200

    Update pub/sub to RCLC (moving required)

--- rcl/int32_publisher_subscriber/main.c
+++ rcl/int32_publisher_subscriber/main.c
@@ -11,81 +11,76 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
-void * trigger_guard_condition(void *args){
-  rcl_guard_condition_t * guard_condition = (rcl_guard_condition_t *)args;
+rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 send_msg;
+std_msgs__msg__Int32 recv_msg;
 
-  while(true){
-    RCSOFTCHECK(rcl_trigger_guard_condition(guard_condition))
-    sleep(1);
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  void last_call_time;
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &send_msg, NULL));
+    printf("Sent: %d\n", send_msg.data);
+    send_msg.data++;
   }
+}
+
+void subscription_callback(const void * msgin)
+{
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;
+	printf("Received: %d\n", msg.data);
 }
 
 int main(int argc, const char * const * argv)
 {
-  rcl_init_options_t options = rcl_get_zero_initialized_init_options();
-  RCCHECK(rcl_init_options_init(&options, rcl_get_default_allocator()))
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
 
-  rcl_context_t context = rcl_get_zero_initialized_context();
-  RCCHECK(rcl_init(argc, argv, &options, &context))
+	// create init_options
+	RCCHECK(rclc_support_init(&support, argc, argv, &allocator));
 
-  rcl_node_options_t node_ops = rcl_node_get_default_options();
-  rcl_node_t node = rcl_get_zero_initialized_node();
-  RCCHECK(rcl_node_init(&node, "int32_publisher_subscriber_rcl", "", &context, &node_ops))
+	// create node
+	rcl_node_t node = rcl_get_zero_initialized_node();
+	RCCHECK(rclc_node_init_default(&node, "int32_publisher_subscriber_rclc", "", &support));
 
-  rcl_guard_condition_t guard_condition = rcl_get_zero_initialized_guard_condition();
-  rcl_guard_condition_options_t guard_condition_options = rcl_guard_condition_get_default_options();
-  RCCHECK(rcl_guard_condition_init(&guard_condition, &context, guard_condition_options))
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"int32_publisher"));
 
-  pthread_t guard_condition_thread;
-  pthread_create(&guard_condition_thread, NULL, trigger_guard_condition, &guard_condition);
+  // create subscriber
+	RCCHECK(rclc_subscription_init_default(
+		&subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"std_msgs_msg_Int32"));
 
-  rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
-  rcl_subscription_t subscription = rcl_get_zero_initialized_subscription();
-  RCCHECK(rcl_subscription_init(&subscription, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/int32_subscriber", &subscription_ops))
+	// create timer,
+	rcl_timer_t timer = rcl_get_zero_initialized_timer();
+	const unsigned int timer_timeout = 1000;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
 
-  rcl_publisher_options_t publisher_ops = rcl_publisher_get_default_options();
-  rcl_publisher_t publisher = rcl_get_zero_initialized_publisher();
-  RCCHECK(rcl_publisher_init(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/int32_publisher", &publisher_ops))
+	// create executor
+	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+	RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
 
-  rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-  RCCHECK(rcl_wait_set_init(&wait_set, 1, 1, 0, 0, 0, 0, &context, rcl_get_default_allocator()))
+	unsigned int rcl_wait_timeout = 1000;   // in ms
+	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &recv_msg, &subscription_callback, ON_NEW_DATA));
 
-  std_msgs__msg__Int32 msg;
-  msg.data = 0;
+	send_msg.data = 0;
+	
+  rclc_executor_spin(&executor);
 
-  rcl_ret_t rc;
-  do {
-    RCSOFTCHECK(rcl_wait_set_clear(&wait_set))
-
-    size_t index_subscription;
-    RCSOFTCHECK(rcl_wait_set_add_subscription(&wait_set, &subscription, &index_subscription))
-
-    size_t index_guardcondition;
-    RCSOFTCHECK(rcl_wait_set_add_guard_condition(&wait_set, &guard_condition, &index_guardcondition))
-
-    RCSOFTCHECK(rcl_wait(&wait_set, RCL_MS_TO_NS(100)))
-
-    if (wait_set.subscriptions[index_subscription]) {
-      std_msgs__msg__Int32 msg;
-
-      rc = rcl_take(wait_set.subscriptions[index_subscription], &msg, NULL, NULL);
-      if (RCL_RET_OK == rc) {
-        printf("I received: [%i]\n", msg.data);
-      }
-    }
-
-    if (wait_set.guard_conditions[index_guardcondition]) {
-      // Use the guard condition trigger to publish in a slower loop
-
-      rc = rcl_publish(&publisher, (const void*)&msg, NULL);
-      if (RCL_RET_OK == rc ) {
-          printf("Sent: '%i'\n", msg.data++);
-      }
-    }
-  } while ( true );
-
-  RCCHECK(rcl_subscription_fini(&subscription, &node))
-  RCCHECK(rcl_node_fini(&node))
-
-  return 0;
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_publisher_fini(&publisher, &node));
+	RCCHECK(rcl_node_fini(&node));
 }
