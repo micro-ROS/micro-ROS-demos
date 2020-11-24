6fb00fcfe777905b00e9bc3355fc9567fdf2870c
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2020-11-24 12:52:23 +0100

    Add publisher/subscription count examples

---
+++ rclc/publisher_count/main.c
@@ -0,0 +1,66 @@
+#include <rcl/rcl.h>
+#include <rcl/graph.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <rcl_interfaces/msg/parameter_event.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+rcl_subscription_t subscription;
+rcl_interfaces__msg__ParameterEvent unused;
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  (void) last_call_time;
+  size_t publisher_count = 0;
+  RCSOFTCHECK(rcl_subscription_get_publisher_count(&subscription, &publisher_count));
+  printf("Publisher count for subscription '/parameter_events': %lu\n", publisher_count);
+}
+
+int main(int argc, const char * const * argv)
+{
+  rcl_allocator_t allocator = rcl_get_default_allocator();
+  rclc_support_t support;
+
+  // create init options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create_node
+  rcl_node_t node = rcl_get_zero_initialized_node();
+  RCCHECK(rclc_node_init_default(&node, "publisher_count_node", "", &support));
+
+  // create subscription
+  RCCHECK(rclc_subscription_init_best_effort(
+    &subscription,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(rcl_interfaces, msg, ParameterEvent),
+    "parameter_events"));
+
+  // create timer
+  rcl_timer_t timer = rcl_get_zero_initialized_timer();
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+
+  unsigned int rcl_wait_timeout = 1000;
+  RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  rclc_executor_spin(&executor);
+
+  RCCHECK(rcl_subscription_fini(&subscription, &node));
+  RCCHECK(rcl_node_fini(&node));
+}
