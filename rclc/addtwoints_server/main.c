ed60a6b63847bbd4723da2d6fb235ee6db768f08
Pablo Garrido <pablogs9@gmail.com>
2020-11-25 10:36:50 +0100

    Fix callback

--- rclc/addtwoints_server/main.c
+++ rclc/addtwoints_server/main.c
@@ -14,15 +14,14 @@
 example_interfaces__srv__AddTwoInts_Request req;
 example_interfaces__srv__AddTwoInts_Response res;
 
-void service_callback(const void * req, rmw_request_id_t * req_id, void * res){
+void service_callback(const void * req, void * res){
   example_interfaces__srv__AddTwoInts_Request * req_in = (example_interfaces__srv__AddTwoInts_Request *) req;
   example_interfaces__srv__AddTwoInts_Response * res_in = (example_interfaces__srv__AddTwoInts_Response *) res;
 
-  printf("Service request value: %d + %d. Seq %d\n", (int) req_in->a, (int) req_in->b, (int) req_id->sequence_number);
+  printf("Service request value: %d + %d.\n", (int) req_in->a, (int) req_in->b;
 
   res_in->sum = req_in->a + req_in->b;
 }
-
 
 int main(int argc, const char * const * argv)
 {
