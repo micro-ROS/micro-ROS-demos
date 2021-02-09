9f7566ddd246e1be4c2cb52999e843e8034d943e
Pablo Garrido <pablogs9@gmail.com>
2021-02-09 13:06:48 +0100

    Update

--- rclc/configuration_example/custom_transports/main.c
+++ rclc/configuration_example/custom_transports/main.c
@@ -18,7 +18,6 @@
 #include <errno.h>
 #include <netdb.h>
 
-#define RMW_UXRCE_TRANSPORT_CUSTOM // Caution!
 #include <rmw_uros/options.h>
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
